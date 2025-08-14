660b57e7d6a4d65ff8826df8cf9e2c7a367465d5
Hallvard Flatberg <hallvard.flatberg@gmail.com>
2025-08-14 10:54:16 +0200

    started on decision tree

---
+++ Iris classification/IrisClassificationDecisionTree.py
@@ -0,0 +1,20 @@
+import kagglehub
+import pandas as pd
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+
+path = kagglehub.dataset_download("arshid/iris-flower-dataset")
+
+csv_path = os.path.join(path, "Iris.csv")
+
+df = pd.read_csv(csv_path)
+
+X = df[["sepal_length", "sepal_width", "petal_length", "petal_width"]]
+y = df["species"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
